a0b5b89f3c196850a8323113c4503384d8e7bff1
herty75 <annorhetty@gmail.com>
2023-04-21 13:19:48 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char kh;
+
+	for (kh = 'z'; kh >= 'a'; kh--)
+		putchar(kh);
+
+	putchar('\n);
+
+	return (0);
+
+}
