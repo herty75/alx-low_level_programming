70e3f3bc3619dc143d2ffabec6f97078a5beb726
herty75 <annorhetty@gmail.com>
2023-06-01 08:31:14 -0700

    100-first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void first(void)_attribute_((constructor));
+void first(void) __attribute__ ((constructor));
 /**
  * first - prints a sentence before the main
  * function is executed
